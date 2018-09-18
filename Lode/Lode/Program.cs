73d552446aeb016528e5841b61b251d80f9d2a3d
David Motúz <David Motz@LAPTOP-694EBULK>
2018-09-18 09:47:05 +0200

    :)

---
+++ Lode/Lode/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lod boat1 = new Lod();
+
+            boat1.changeDirection();
+            string pozice = boat1.getDirection();
+            Console.WriteLine(pozice);
+        }
+    }
+}
---
+++ Lode/Lode/lod.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lode
+{
+    class Lod
+    {
+        private int locY = 5;
+        private int locX = 5;
+        private int lenght = 3;
+        private bool direction = true; // true = horizontální, false = vertikální
+
+        public void changeDirection()
+        {
+            if (direction == true)
+            {
+                direction = false;
+            }
+            else
+            {
+                direction = true;
+            }
+
+        }
+
+        public string getDirection()
+        {
+            if (direction == true)
+            {
+                return "horizontal";
+            }
+            else
+            {
+                return "vertical";
+            }
+        }
+    }
+}
+
